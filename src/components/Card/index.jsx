489f05663492fc1026b53ead25ab3f3dd8445554
Roberta Fontes <robertafontesds@gmail.com>
2024-03-01 16:28:19 -0300

    first commit

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,17 @@
+import styles from "./Card.module.css"
+export function Card({ username, description, image, route }) {
+  return (
+    <a href={route}>
+      <div className={styles.card}>
+        <div className={styles.details}>
+          <img src={image} alt="" />
+          <div>
+            <p>{username}</p>
+            <p>{description}</p>
+          </div>
+        </div>
+        <span className="material-symbols-outlined">chevron_right</span>
+      </div>
+    </a>
+  )
+}
